1e03bd9d1b96bd024a1b634279caa13b4184d751
cjgamble21 <cjgamble21@gmail.com>
2023-05-12 15:51:58 -0500

    Constructor, normalize, and magnitude methods added (no tests yet)

--- src/Vector2.cpp
+++ src/Vector2.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "Vector2.h"
+
+/*
+ * Divide x & y by magnitude to squish components into unit space [0, 1]
+ */
+
+crn::Vector2 &crn::Vector2::normalize() {
+    float v_magnitude = magnitude();
+    x_comp /= v_magnitude;
+    y_comp /= v_magnitude;
+    return *this;
+}
+
+/*
+ * |V| = sqrt( x^2 + y^2 )
+ */
+
+float crn::Vector2::magnitude() const {
+    return static_cast<float>(sqrt(pow(x_comp, 2) + pow(y_comp, 2)));
+}
--- src/Vector2.h
+++ src/Vector2.h
@@ -5,10 +5,25 @@
 #ifndef CORUNDUMENGINE_VECTOR2_H
 #define CORUNDUMENGINE_VECTOR2_H
 
+#include <cmath>
 
-class Vector2 {
+namespace crn {
 
-};
+    struct Vector2 {
+        float x_comp;
+        float y_comp;
+
+        constexpr Vector2() : x_comp(0.0), y_comp(0.0) {}
+
+        constexpr Vector2(const float& x, const float& y) : x_comp(x), y_comp(y) {}
+
+        Vector2& normalize();
+
+        inline float magnitude() const;
+
+    };
+
+}
 
 
 #endif //CORUNDUMENGINE_VECTOR2_H
